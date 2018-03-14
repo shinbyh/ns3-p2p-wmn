2c62859615282aee176428c74e1713263a24a0e1
Byoungheon Shin <shinbyh@gmail.com>
2018-03-14 23:44:16 +0900

    Resolved a bug of calculating source rt dscv count

--- integrate_stat_src_rt.sh
+++ integrate_stat_src_rt.sh
@@ -4,10 +4,10 @@
 # All variables below will be provided by environment variables.
 ######################################################################
 #EXP_NAME=$1
-#APPS="7apps"
+#APPS="6apps 7apps 8apps"
 #PKTS="250pkts"
 #FLOWSETS="0 1 2 3 4 5 6 7 8 9"
-#SCHEMES="0 1 2"
+#SCHEMES="0 2"
 #DIR_PREFIX=$HOME"/exp/ns-3"
 #APPCONFIG_DIR="./apps_config"
 #topologyConfigFile="topology.config"
